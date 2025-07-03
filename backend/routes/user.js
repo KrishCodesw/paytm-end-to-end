7dc707c2abaf297627495af0ded28c696fad4204
KrishCodesw <krishjain.w@gmail.com>
2025-07-03 11:39:24 +0530

    signup and sigin routes

---
+++ backend/routes/user.js
@@ -0,0 +1,98 @@
+const express=require('express')
+const router=express.Router();
+const zod=require('zod')
+const {User}=require("../db")
+const jwt=require("jsonwebtoken")
+const { JWT_SECRET } = require("../config");
+const bcrypt = require('bcryptjs');
+
+const signupBody = zod.object({
+    username: zod.string().email(),
+	firstname: zod.string(),
+	lastname: zod.string(),
+	password: zod.string()
+})
+
+
+
+
+
+router.post("/signup",async(req,res)=>{
+   const {success}=signupBody.safeParse(req.body)
+   if(!success){
+    return res.status(411).json({
+        message:"Invalid request body",
+    })
+   }
+     const existingUser = await User.findOne({
+        username: req.body.username
+    })
+     if (existingUser) {
+        return res.status(411).json({
+            message: "Email already taken/Incorrect inputs"
+        })
+    }
+
+const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    const user=await User.create({
+        username:req.body.username ,
+        password:hashedPassword,
+        firstname:req.body.firstname ,
+        lastname:req.body.lastname
+    })
+    const userId = user._id;
+
+    const token=jwt.sign({userId},JWT_SECRET);
+    res.json({
+        message:"User created successfully",
+        token:token
+    })
+
+
+})
+
+
+
+const signinBody = zod.object({
+    username: zod.string().email(),
+	password: zod.string()
+})
+
+router.post("/signin",async(req,res)=>{
+    const { success } = signinBody.safeParse(req.body)
+        if (!success) {
+        return res.status(411).json({
+            message: "Incorrect inputs"
+        })
+    }
+
+    const user=await User.findOne({
+        username:req.body.username,
+  
+    })
+    if (!user) {
+    return res.status(411).json({ message: "User not found" });
+  }
+
+  const isPasswordCorrect=await bcrypt.compare(req.body.password,user.password)
+   if (!isPasswordCorrect) {
+    return res.status(411).json({ message: "Invalid password" });
+  }
+
+
+        const token=jwt.sign({userId:user._id},JWT_SECRET)
+        res.json({token:token})
+        return
+    
+     
+})
+
+
+
+
+
+
+
+
+module.exports=router
