a89bdba4d45a45f6f5af81de46f1b7e47484b835
KrishCodesw <krishjain.w@gmail.com>
2025-07-03 19:08:44 +0530

    Added the two most important routes ,The balance route and the money transfer route using the concept of transaction

---
+++ backend/routes/accounts.js
@@ -0,0 +1,104 @@
+const express=require('express');
+const { authMiddleware } = require('../midddleware');
+const { Account } = require('../db');
+const mongoose=require("mongoose")
+const router=express.Router();
+
+router.get("/balance",authMiddleware,async(req,res)=>{
+        const account=await Account.findOne({
+            userId:req.userId
+        })
+        res.json({balance:account.balance})
+})
+
+// router.post("/transfer",authMiddleware,async(req,res)=>{
+//         const {amount,to}=req.body;
+//         const account=await Account.findOne({
+//             userId:req.userId
+//         })
+//         if(account.balance<amount){
+//             res.json({
+//                 message:"Insufficient balance"
+//             })
+//         }
+//         const toAccount=await Account.findOne({
+//             userId:to
+//         })
+//             if (!toAccount) {
+//         return res.status(400).json({
+//             message: "Invalid account"
+//         })
+//     }
+
+//     await account.updateOne({userId:req.userId},{$inc:{balance:-amount}})
+//     await account.updateOne({userId:to},{$inc:{balance:amount}})
+
+//     res.json({
+//         massage:"Amount transferred to the respective account"
+//     })
+// })
+
+// THIS IS A REALLY BAD APPROACH ---> IF BACKEND/DB GOES DOWN, THE TRANSACTION WILL BE LOST AND BALANCE WILL TURN NEGATIVE
+
+
+
+router.post("/transfer",authMiddleware,async(req,res)=>{
+        const session=await mongoose.startSession();
+
+
+        session.startTransaction()
+
+        const {amount,to}=req.body;
+        const account=await Account.findOne({
+            userId:req.userId
+        }).session(session)
+
+
+
+        if(!account||account.balance<amount){
+             await session.abortTransaction();
+         return   res.status(400).json({
+                message:"Insufficient balance"
+            })
+        }
+
+
+        const toAccount=await Account.findOne({
+            userId:to
+        }).session(session)
+            if (!toAccount) {
+                 await session.abortTransaction();
+        return res.status(400).json({
+            message: "Invalid account"
+        })
+    }
+
+    await account.updateOne({userId:req.userId},{$inc:{balance:-amount}})
+    await account.updateOne({userId:to},{$inc:{balance:amount}})
+     await session.commitTransaction();
+
+    res.json({
+        massage:"Amount transferred to the respective account"
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports=router
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const userRoutes=require('./user')
 const router=express.Router();
-
+const accountRoutes=require("./accounts")
 router.use("/user",userRoutes)
+router.use("/account",accountRoutes)
 module.exports=router
