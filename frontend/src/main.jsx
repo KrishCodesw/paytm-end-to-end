d587e06efdf0874c094eb0a809b6ac03aa8d3933
KrishCodesw <krishjain.w@gmail.com>
2025-07-07 02:46:05 +0530

    Auth import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+// import { AuthProvider } from "../context/AuthContext.jsx";
 import { AuthProvider } from "../context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
