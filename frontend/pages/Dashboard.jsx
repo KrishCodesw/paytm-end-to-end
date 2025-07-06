22181342058d036ef26b69e2983ecff4c89af314
KrishCodesw <krishjain.w@gmail.com>
2025-07-06 23:32:36 +0530

    Built the dashboard and fetched user balance

--- frontend/pages/Dashboard.jsx
+++ frontend/pages/Dashboard.jsx
@@ -1,7 +1,36 @@
 import React from "react";
-
+import { useState, useEffect } from "react";
 const Dashboard = () => {
-  return <div></div>;
+  const [balance, setBalance] = useState();
+
+  useEffect(() => {
+    const fetchBalance = async () => {
+      const token = localStorage.getItem("token");
+      const res = await fetch("http://localhost:3000/api/v1/account/balance", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const json = await res.json();
+      if (res.ok) setBalance(json.balance);
+    };
+    fetchBalance();
+  }, []);
+
+  return (
+    <div>
+      <div className="p-8">
+        <h2 className="text-xl text-white font-bold mb-4">Dashboard</h2>
+        {balance === null ? (
+          <p className="text-xl text-white font-bold mb-4">Loading...</p>
+        ) : (
+          <p className="text-xl text-white font-bold mb-4">
+            Your balance: ₹{balance}
+          </p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
