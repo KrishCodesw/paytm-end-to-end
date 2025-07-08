80ad081d5ec00833440ee66a0746df96f6b2f14c
KrishCodesw <krishjain.w@gmail.com>
2025-07-09 00:10:10 +0530

    CORS

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,12 @@ require("dotenv").config();
 
 
 const app=express();
-app.use(cors());
 
+app.use(cors({
+  origin: "https://paytm-roan.vercel.app/",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true, // only if using cookies
+}));
 app.use(express.json())
 app.use('/api/v1',rootRouter)
 
